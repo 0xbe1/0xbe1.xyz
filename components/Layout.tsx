56102aeaef0690548f49c3b7fd73d30e85296e6f
0xbe1 <0xbetrue@gmail.com>
2022-06-18 06:54:27 +0800

    mvp

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Link from "next/link";
+import { ReactElement } from "react";
+const Layout = ({ children }: { children: ReactElement }) => {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <Head>
+        <title>0xbe1.xyz</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/github-dark.min.css"
+        ></link>
+      </Head>
+
+      <div className="flex items-center space-x-4 py-4">
+        <Link href="/">
+          <a className="font-bold">0xbe1</a>
+        </Link>
+        <Link href="/writing">
+          <a>Writing</a>
+        </Link>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+};
+export default Layout;
---
+++ pages/writing/[slug].tsx
@@ -0,0 +1,67 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { ParsedUrlQuery } from "querystring";
+import { GetStaticPaths, GetStaticProps } from "next";
+import rehypeHighlight from "rehype-highlight";
+
+interface Params extends ParsedUrlQuery {
+  slug: string;
+}
+
+interface Props {
+  frontMatter: { [key: string]: any };
+  mdxSource: MDXRemoteSerializeResult;
+}
+
+const components = {};
+
+export default function Post({ frontMatter, mdxSource }: Props) {
+  return (
+    <div className="prose">
+      <h1>{frontMatter.title as string}</h1>
+      <MDXRemote {...mdxSource} components={components} />
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".mdx", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (
+  context
+) => {
+  const { slug } = context.params!;
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".mdx"),
+    "utf-8"
+  );
+
+  const { data: frontMatter, content } = matter(markdownWithMeta);
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [rehypeHighlight],
+    },
+  });
+
+  return {
+    props: {
+      frontMatter,
+      mdxSource,
+    },
+  };
+};
