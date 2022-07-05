d03ac015efb01ce47e912f4d535c4c131585146b
0xbe1 <0xbetrue@gmail.com>
2022-07-06 00:26:35 +0800

    add tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        data-token="EB8HA3B79SXK"
+        async
+        src="https://cdn.splitbee.io/sb.js"
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
