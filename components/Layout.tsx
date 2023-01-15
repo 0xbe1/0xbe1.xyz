acb7304a9fa1654bae6f8d3f4473cdc0148aa4a1
0xbe1 <0xbetrue@gmail.com>
2023-01-15 20:35:10 +0800

    add _document

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
 import { ReactElement } from "react";
+
 const Layout = ({ children }: { children: ReactElement }) => {
   return (
     <div className="max-w-3xl mx-auto p-4">
       <Head>
         <title>0xbe1 | A crypto dev</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/github-dark.min.css"
-        ></link>
       </Head>
-
       <div className="flow-root flex-row mb-10">
         <div className="float-left space-x-4">
           <Link href="/" className="font-bold">
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/github-dark.min.css"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
